f38ee8f29fb1fc8da61a9f4aff30a75f1d1d46a3
lichaowow6666 <lichaowow6666@gmail.com>
2017-06-17 10:11:49 +0800

    extra commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by ChaoLi on 16.06.17.
+//  Copyright © 2017 ChaoLi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    // just some words
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";
+    return 0;
+}
